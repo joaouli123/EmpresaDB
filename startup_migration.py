947997ddcc8703f1b6a227f8b9cb0d329ec5d9b1
joao lucas <jl.uli1996@gmail.com>
2026-06-22 22:05:14 -0300

    fix: startup_migration com timeout e sempre inicia gunicorn (; em vez de &&)

--- startup_migration.py
+++ startup_migration.py
@@ -1,12 +1,14 @@
-import os, psycopg2, sys
+import os, psycopg2, sys, time
 
 dsn = os.environ.get('DATABASE_URL')
 if not dsn:
     print('startup_migration: DATABASE_URL nao definida, pulando...')
     sys.exit(0)
 
+time.sleep(5)
+
 try:
-    conn = psycopg2.connect(dsn)
+    conn = psycopg2.connect(dsn, connect_timeout=10)
     conn.autocommit = True
     cur = conn.cursor()
     with open('scripts/fix_trigram_indexes.sql', 'r') as f:
